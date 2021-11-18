9b003e5b24b0d4420dbba1393037ed1f1a4e7fe4
tinxo3 <martinalelopez@outlook.com>
2021-11-17 21:02:58 -0300

    lvl 1

--- Portal.gd
+++ Portal.gd
@@ -1,8 +1,8 @@
 extends Area
 
-export (String) var escena
+#export (String) var escena
 
-func _on_Portal_body_entered(body):
-	if body.name == "Player":
-		get_tree().change_scene(res://levels/Level1.tscn)
-	pass # Replace with function body.
+#func _on_Portal_body_entered(body):
+	#if body.name == "Player":
+		#get_tree().change_scene(res://levels/Level1.tscn)
+	#pass # Replace with function body.
