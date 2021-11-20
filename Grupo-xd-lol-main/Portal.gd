d6e65989cfbabc7dc88b41ae78aa8623001d31c3
tinxo3 <martinalelopez@outlook.com>
2021-11-20 16:06:58 -0300

    Carpeta eliminada

--- Grupo-xd-lol-main/Portal.gd
+++
@@ -1,8 +0,0 @@
-extends Area
-
-#export (String) var escena
-
-#func _on_Portal_body_entered(body):
-	#if body.name == "Player":
-		#get_tree().change_scene(res://levels/Level1.tscn)
-	#pass # Replace with function body.
--- Grupo-xd-lol-main/scenes/CameraAssembly.gd
+++
@@ -1,9 +0,0 @@
-extends Position3D
-
-
-export var target_path : NodePath
-onready var target = get_node(target_path)
-onready var offset = target.transform.origin - transform.origin
-
-func _process(delta):
-	transform.origin = target.transform.origin + offset
