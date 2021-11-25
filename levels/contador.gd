6c01d4e4f23f60620480b604ee3e25cfdfd78a1b
Kogner1 <sansanramos96@gmail.com>
2021-11-24 22:06:20 -0300

    Fonts coins

--- levels/contador.gd
+++ levels/contador.gd
@@ -3,12 +3,12 @@ extends Label
 var coins = 0 
 
 func _ready():
-	text = String (coins)
+	text = str (coins)
 
 
 
 
 
-func _on_coinCollected():
+func _on_coinCollected(): 
 	coins = coins + 1
-	text = String (coins)
+	text = str (coins)
